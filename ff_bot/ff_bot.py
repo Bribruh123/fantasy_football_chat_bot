a9f1cb5177f8263be0b26696786bc4ab249c9906
Brian Hoch (brhoch) <brhoch@cisco.com>
2021-09-23 12:51:16 -0500

    activity

--- ff_bot/ff_bot.py
+++ ff_bot/ff_bot.py
@@ -142,7 +142,7 @@ def get_activity(league, week=None):
             actions_player = action[2].name
             action_datetime = action[4]- datetime.timedelta(hours=5)
             if (str(current).split(":")[0:2] == str(action_datetime).split(":")[0:2]):
-                activity_txt.append( "" + action[0].team_name +  " " + action[1] + " " + action[2].name)# + " at "  + str(str(current.stimezone(timezone('US/Central')).split(":")[0:2].join()))
+                activity_txt.append( "" + action[0].team_name + "( " + action_owner +  ") " + action[1] + " " + action[2].name)# + " at "  + str(str(current.stimezone(timezone('US/Central')).split(":")[0:2].join()))
                 print("\t" + action_team ,  " " , action_name , " " , actions_player , " @ " , str(action_datetime).split(' ')[1].split('.')[0] , " CDT")
                 print("\t - NEW - Sending")
      
