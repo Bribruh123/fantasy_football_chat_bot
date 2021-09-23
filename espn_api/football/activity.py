048b1848d7c81c919e384144c12b868f387c4177
Brian Hoch (brhoch) <brhoch@cisco.com>
2021-09-23 09:33:08 -0500

    activity

--- espn_api/football/activity.py
+++ espn_api/football/activity.py
@@ -42,6 +42,8 @@ class Activity(object):
             self.actions.append((team, action, player, bid_amount, self.date))
 
     def __repr__(self):
+        for action in self.actions:
+            print(action)
         return 'Activity(' + ' '.join("(%s,%s,%s,%s)" % tup[0:4] for tup in self.actions) + ')'
 
 
