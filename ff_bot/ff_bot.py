1d40546af08a5b08fa4fe4c6d3e2382ec0c7a63b
Brian Hoch (brhoch) <brhoch@cisco.com>
2021-09-24 09:11:48 -0500

    activity_final_test

--- ff_bot/ff_bot.py
+++ ff_bot/ff_bot.py
@@ -128,8 +128,8 @@ def get_activity(league, week=None):
     #Gets recent activity
     
     activity = league.recent_activity(size=10)
-    #current = datetime.datetime.now() - datetime.timedelta(hours=5)
-    current = datetime.datetime(2021, 9, 23, 10, 30, 0)
+    current = datetime.datetime.now() - datetime.timedelta(hours=5)
+    #current = datetime.datetime(2021, 9, 23, 10, 30, 0)
     
     most_recent = activity[0].actions[0][4] - datetime.timedelta(hours=5)
     print("Current time:                 " , str(current).split('.')[0] , "CDT" )
